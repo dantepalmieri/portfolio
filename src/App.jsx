807bff34ae80867a654f2736e6ce51be4059df98
Dante <123109249+dantepalmieri@users.noreply.github.com>
2024-10-13 22:07:09 +0000

    changed the App.jsx file site props

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Dante Palmieri",
+  title: "Computer Engineering Student",
+  email: "djp393@nau.edu",
+  gitHub: "dantepalmieri",
+  instagram: "dantepalmierii",
+  linkedIn: "Dante Palmieri",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "tnead_",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
