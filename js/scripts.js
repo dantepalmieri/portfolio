958af9e16c5d88bd1cb15f7de38b752842f1804a
dantepalmieri <palmieridante4@gmail.com>
2025-03-16 17:06:22 -0700

    added resume to repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-// This file is intentionally left blank.
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
